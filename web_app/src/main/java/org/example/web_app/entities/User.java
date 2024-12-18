083e7a695faf7af05bda92ebd02d8250c9d9b923
plaffyyy <k.zimin@innopolis.university>
2024-12-19 00:28:28 +0300

    feat(user database): create user database

---
+++ web_app/src/main/java/org/example/web_app/entities/User.java
@@ -0,0 +1,26 @@
+package org.example.web_app.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public final class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column()
+    private String photoUrl;
+}
